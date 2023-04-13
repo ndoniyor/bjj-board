35c3f852c31bb40f7df719befdab634686261fbe
ndoniyor <dnimatullo@gmail.com>
2023-04-12 20:14:49 -0400

    set up route and component files

---
+++ components/css/HomeCard.css
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomeCard() {
+  return (
+    <div>HomeCard</div>
+  )
+}
---
+++ routes/SimpleView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SimpleView() {
+  return (
+    <div>SimpleView</div>
+  )
+}
