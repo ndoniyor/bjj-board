4e6c14e379a80ea6cab87529b2b220190dddf449
ndoniyor <dnimatullo@gmail.com>
2023-04-19 18:45:32 -0400

    added component to display simple post info

--- components/SimpleCard.jsx
+++ components/SimpleCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
-export default function SimpleCard() {
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './css/SimpleCard.css'
+export default function SimpleCard(props) {
+  useEffect(()=>{
+    console.log(props)
+  })
   return (
-    <div>SimpleCard</div>
+    <div className='SimpleCard'>
+      <Link to={`/gallery/${props.id}`} className='simple-title'>{props.title}</Link>
+      <small className='simple-category'>{props.category}</small>
+      <p className='simple-body'>{props.body}</p>
+    </div>
   )
 }
