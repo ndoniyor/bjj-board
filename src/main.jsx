e3f7da449264a5b39c6c32e68015108fafc18033
ndoniyor <dnimatullo@gmail.com>
2023-04-19 18:47:29 -0400

    sync'd new route with main

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SimpleView from '../routes/SimpleView'
-import CreateView from '../routes/CreateView'
-import Layout from '../routes/Layout'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SimpleView from "../routes/SimpleView";
+import CreateView from "../routes/CreateView";
+import DetailView from "../routes/DetailView";
+import Layout from "../routes/Layout";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index={true} element={<App />}/>
-          <Route index={false} path="/gallery/" element={<SimpleView/>}/>
-          <Route index={false} path="/create/" element={<CreateView/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index={true} element={<App />} />
+          <Route index={false} path="/gallery/*">
+            <Route index={true} element={<SimpleView />} />
+            <Route path=":id" element={<DetailView />} />
+          </Route>
+          <Route index={false} path="/create/" element={<CreateView />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
