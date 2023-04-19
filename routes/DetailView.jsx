74ced2a266b7be7679e0b39ba75c0552cdbefad1
ndoniyor <dnimatullo@gmail.com>
2023-04-19 18:47:08 -0400

    added route for in-depth post view

--- routes/DetailView.jsx
+++ routes/DetailView.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import DetailCard from '../components/DetailCard'
 
 export default function DetailView() {
   return (
-    <div>DetailView</div>
+    <div className="DetailView">
+      <DetailCard />
+    </div>
   )
 }
