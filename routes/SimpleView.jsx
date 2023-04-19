7fd3fec70210a1e0ffcf45c618648600e5700e7d
ndoniyor <dnimatullo@gmail.com>
2023-04-19 18:45:46 -0400

    added route to post gallery

--- routes/SimpleView.jsx
+++ routes/SimpleView.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { supabase } from '../src/client';
+import SimpleCard from '../components/SimpleCard';
 
 export default function SimpleView() {
+  const [data,setData] = useState([])
+  const getAll = async () => {
+    const data_pre = await supabase.from("posts").select("*");
+    setData(data_pre.data);
+  }
+
+  useEffect(()=>{
+    getAll();
+  },[])
+
   return (
-    <div>SimpleView</div>
+    <div className="SimpleView">
+      {data && data.map((post)=>
+        <SimpleCard 
+          title={post.title}
+          category={post.category}
+          body={post.body}
+          id={post.id}
+        />
+      )}
+    </div>
   )
 }
