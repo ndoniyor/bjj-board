6c113951751820844f8e45a15f948c3ef7a2cb01
ndoniyor <dnimatullo@gmail.com>
2023-04-23 16:51:30 -0400

    Revert "added component for in-depth post view"
    This reverts commit 300c37337f9b55e574c413de6e13a6a2b0f44fbc.

--- components/DetailCard.jsx
+++ components/DetailCard.jsx
@@ -1,17 +1,7 @@
-import React from "react";
-import './css/DetailCard.css'
-export default function DetailCard(props) {
+import React from 'react'
+
+export default function DetailCard() {
   return (
-    <div className="DetailCard">
-      <div>
-        <p>{props.upvotes}</p>
-      </div>
-      <div>
-        <h2>{props.title}</h2>
-        <small>{props.category}</small>
-        <p>{props.body}</p>
-        <small>{props.skills}</small>
-      </div>
-    </div>
-  );
+    <div>DetailCard</div>
+  )
 }
