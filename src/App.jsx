c029ed0c85326e19a3a53b34216b9b4dd6bee653
ndoniyor <dnimatullo@gmail.com>
2023-04-24 15:55:34 -0400

    added icon to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { GiBlackBelt } from 'react-icons/gi'
 
 import React from 'react'
 
 export default function App() {
   return (
     <div className="text-center">
-      <h1>Welcome to the BJJ Board</h1>
+      <div className="header-text">
+        <GiBlackBelt 
+        fill="white" 
+        size={48}
+        />
+        <h1>Welcome to the BJJ Board</h1>
+      </div>
+
       <Button variant="outline-light" color='#fffff' as={Link} to="/gallery" className="mt-3">Begin your journey</Button>
     </div>
   )
