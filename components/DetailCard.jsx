415f79b9df0bf5eaa4e40a94af368c9d7dbc1dcd
ndoniyor <dnimatullo@gmail.com>
2023-04-24 00:31:46 -0400

    added new view for more in-depth look at post

--- components/DetailCard.jsx
+++ components/DetailCard.jsx
@@ -1,17 +1,48 @@
-import React from "react";
 import './css/DetailCard.css'
+import {Button} from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import {Link} from 'react-router-dom';
+import { supabase } from "../src/client";
+
 export default function DetailCard(props) {
+  const [upvotes, setUpvotes] = useState(0);
+
+  const updateUpvotes = async()=> {
+    await supabase
+    .from("posts")
+    .update({upvotes: upvotes})
+    .eq('id',props.id);
+  }
+
+  const handleUpvote = async () =>{
+    setUpvotes(upvotes+1);
+    await updateUpvotes();
+  }
+
+  useEffect(()=>{
+    if(props.upvotes){
+      setUpvotes(Number(props.upvotes));
+    }  
+  },[props.upvotes])
+
   return (
-    <div className="DetailCard">
-      <div>
-        <p>{props.upvotes}</p>
-      </div>
-      <div>
-        <h2>{props.title}</h2>
-        <small>{props.category}</small>
-        <p>{props.body}</p>
-        <small>{props.skills}</small>
+    <div className="card mb-3">
+      <div className="card-body">
+        <div className="d-flex justify-content-between align-items-center mb-2">
+          <h2 className="card-title mb-0">{props.title}</h2>
+          <div className="text-muted">{props.category}</div>
+        </div>
+        <p className="card-text">{props.body}</p>
+        <div className="d-flex justify-content-between align-items-center">
+          <Button variant="outline-dark" as={Link} to={`/edit/${props.id}`} className="mt-3">Edit Post</Button>
+          <small className="text-muted">{props.skills}</small>
+          <div>
+            <span className="me-2">{upvotes}</span>
+            <Button variant="outline-dark" className="mt-3" onClick={handleUpvote}>Upvote</Button>
+          </div>
+        </div>
       </div>
     </div>
+
   );
 }
