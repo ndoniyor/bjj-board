62b6d2d69da4b32e46843c2bf0058c999260a1fd
ndoniyor <dnimatullo@gmail.com>
2023-04-24 15:55:07 -0400

    bootstrapification

--- routes/Layout.jsx
+++ routes/Layout.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet, Link } from 'react-router-dom';
+import {AiOutlineHome} from 'react-icons/ai'
 
 export default function Layout() {
   return (
     <div>
       <Navbar className="px-3 py-2" bg="dark" variant="dark" fixed="top">
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <AiOutlineHome />
+        </Navbar.Brand>
         <Nav>
-          <Nav.Link href="/gallery">View Posts</Nav.Link>
-          <Nav.Link href="/create">Create</Nav.Link>
+          <Nav.Link as={Link} to="/gallery">View Posts</Nav.Link>
+          <Nav.Link as={Link} to="/create">Create</Nav.Link>
         </Nav>
       </Navbar>
       <Outlet />
